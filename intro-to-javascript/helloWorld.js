a8f6c2571eb13b50c4e9414f367e69b3d2c450e9
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-02-01 23:37:21 +0100

    refactor: revise first chapter comments

--- intro-to-javascript/helloWorld.js
+++ intro-to-javascript/helloWorld.js
@@ -31,3 +31,22 @@ try {
 } catch {
     console.log('This would raise a runtime Error')
     console.log('TypeError: Assignment to constant variable.')
+}
+// Learn more about this at: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
+// Let’s take one more glance at the concepts we just learned:
+
+// - Data is printed, or logged, to the console, a panel that displays messages, with console.log().
+// - We can write single-line comments with // and multi-line comments between /* and */.
+// - There are 7 fundamental data types in JavaScript: strings, numbers, booleans, null, undefined, symbol, and object.
+// - Numbers are any number without quotes: 23.8879
+// - Strings are characters wrapped in single or double quotes: 'Sample String'
+// - The built-in arithmetic operators include +, -, *, /, and %.
+// - Objects, including instances of data types, can have properties, stored information. The properties are denoted with a . after the name of the object, for example: 'Hello'.length.
+// - Objects, including instances of data types, can have methods which perform actions. Methods are called by appending the object or instance with a period, the method name, and parentheses. For example: 'hello'.toUpperCase().
+// - We can access properties and methods by using the ., dot operator.
+// - Built-in objects, including Math, are collections of methods and properties that JavaScript provides.
+
+// Here are a few more resources to add to your toolkit:
+
+// Codecademy Docs: JavaScript https://www.codecademy.com/resources/docs/javascript
+// Codecademy Workspaces: JavaScript https://www.codecademy.com/workspaces/new
