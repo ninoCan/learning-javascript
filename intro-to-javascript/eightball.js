446b1cbcc160faac7ccc5ad9a8a0a3c862527630
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-02-05 11:54:43 +0100

    feat: add 8ball project

--- intro-to-javascript/eightball.js
+++ intro-to-javascript/eightball.js
@@ -1,30 +1,50 @@
-// store userName
-let userName = '';
-console.log(userName ? `Hello, ${userName}!` : 'Hello!');
-
-let userQuestion = "This should be the question for the 8 ball";
-console.log(userQuestion);
-
-// generate the eightball answer
-let randomNumber = Math.floor(Math.random() * 8);
-let eightBall = '';
-
-switch (randomNumber) {
-        case 0:
-            eightBall = 'It is certain'; break;
-            case 1:
-            eightBall = 'It is decidedly so'; break;
-            case 2:
-            eightBall = 'Reply hazy try again'; break;
-            case 3:
-            eightBall = 'Cannot predict now'; break;
-            case 4:
-            eightBall = 'Do not count on it'; break;
-            case 5:
-            eightBall = 'My sources say no'; break;
-            case 6:
-            eightBall = 'Outlook not so good'; break;
-            case 7:
-            eightBall = 'Signs point to yes'; break;
+function main() {
+  let name = askUserName()
+  promptUserQuestion()
+  console.log(answer());
+  greet(name)
 }
-console.log(eightBall);
+
+const askUserName = () => {
+  // store userName
+  let userName = prompt("What's your name?");
+  console.log(userName ? `Hello, ${userName}!` : 'Hello!');
+  return userName
+}
+
+const greet = (name = '') => {
+  console.log(
+    (name) ? `Goodbye, ${name}!` : "Goodbye!"
+  )
+}
+
+const promptUserQuestion = () => {
+  let userQuestion = prompt("What do you want to ask the ball?");
+  console.log(userQuestion);
+}
+
+const _pick_answer = () => {
+}
+const answer = () => {
+  // generate the eightball answer;
+  let randomNumber = Math.floor(Math.random() * 8);
+
+  switch (randomNumber) {
+    case 0:
+      return 'It is certain'; break;
+    case 1:
+      return 'It is decidedly so'; break;
+    case 2:
+      return 'Reply hazy try again'; break;
+    case 3:
+      return 'Cannot predict now'; break;
+    case 4:
+      return 'Do not count on it'; break;
+    case 5:
+      return 'My sources say no'; break;
+    case 6:
+      return 'Outlook not so good'; break;
+    case 7:
+      return 'Signs point to yes'; break;
+  }
+}
