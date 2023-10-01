21429838e5a22159be2f143ea3d928a42da7bcfa
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-10-01 16:10:21 +0200

    Remove unused files

--- full-stack/react/animals-fun-facts/src/index.tsx
+++ full-stack/react/animals-fun-facts/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AnimalFacts from './components/AnimalFacts/AnimalFacts';
 
@@ -10,7 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <AnimalFacts  title=''/>
   </React.StrictMode>
 );
