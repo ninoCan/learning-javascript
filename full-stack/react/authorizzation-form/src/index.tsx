a396ceacb8b45fad43dc1aea27de4203e8ff5ceb
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-10-06 00:18:23 +0200

    Add Contact.tsx

--- full-stack/react/authorizzation-form/src/index.tsx
+++ full-stack/react/authorizzation-form/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Contact from './Contact'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Contact />
   </React.StrictMode>
 );
 
