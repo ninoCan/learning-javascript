18b30d72a0fe84693308eb3b78eb14e51db4fb19
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-10-06 14:47:51 +0200

    Add styles and minimal auth layer plus refactoring into compontents

--- full-stack/react/authorizzation-form/src/index.tsx
+++ full-stack/react/authorizzation-form/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Contact from './Contact'
+import Contact from './components/Contact/Contact';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
