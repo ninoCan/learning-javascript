ec0ade4128cadbf6b8001f4eba13bc404cca4641
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-10-06 15:55:10 +0200

    Debug: move login logic to Contact layer

--- full-stack/react/authorizzation-form/src/components/Contact/Contact.tsx
+++ full-stack/react/authorizzation-form/src/components/Contact/Contact.tsx
@@ -4,9 +4,28 @@ import LoginForm from '../LoginForm/LoginForm';
 
 function Contact() {
   const [authorized, setAuthorized] = useState<boolean>(false);
+  const correctPassword = "bigKahuha";
+
+  function handleSubmit(event: FormEvent<HTMLFormElement> | any): void {
+    const enteredPassword: string = event.target.querySelector(
+      'input[type="password"]').value;
+    const auth = enteredPassword == correctPassword;
+    setAuthorized(auth)
+  }
+
+  const login = (
+      <div id="authorization">
+        <h1>Enter the Password</h1>
+        <form action='#' onSubmit={handleSubmit}>
+          <input type="password" placeholder='Password'></input>
+          <input type="submit" />
+        </form>
+      </div>
+    );
 
   if (!authorized) {
-    return <LoginForm auth={authorized}/>
+    // return <LoginForm auth={authorized}/>
+    return <>{login}</>
   }
   return <ContactDetails />;
 }
