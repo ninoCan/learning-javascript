2fcd26ed8682d8dec9e88161df4fb16b8271659f
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-10-08 01:42:30 +1400

    Add behavior for play_circle button

--- full-stack/react/player/src/components/PlayableButtons/PlayableButtons.tsx
+++ full-stack/react/player/src/components/PlayableButtons/PlayableButtons.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './style.css'
 
 function PlayableButtons() {
+  const [playing, setPlaying] = useState<boolean>(false)
+
+  function togglePlaying() {
+    return setPlaying(!playing);
+  }
+
+  function stopPlaying() {
+    return setPlaying(false);
+  }
+
   return (
     <>
       <span className="material-symbols-outlined">
         fast_rewind
       </span>
-      <span className="material-symbols-outlined">
-        play_circle
+      <span className="material-symbols-outlined" onClick={togglePlaying}>
+        {(playing) ? "pause" : "play_circle"}
       </span>
       <span className="material-symbols-outlined">
         fast_forward
